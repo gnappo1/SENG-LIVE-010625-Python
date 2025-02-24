a55eec038c31648d76440d1efea4881d99f691de
Matteo Piccini <gnappo.1@live.it>
2025-02-24 08:49:33 -0800

    add starter branch

---
+++ 04_python_oop_pt_2/lib/cat.py
@@ -0,0 +1,12 @@
+# import ipdb
+
+# 7. ✅. Create a subclass of Pet called Cat
+# import Pet from lib.pet
+
+from pet import Pet
+
+# 7. ✅. Create a subclass of Pet called Cat
+
+# import Pet from lib.pet
+class Cat:
+    pass
